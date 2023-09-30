76c9cc76e25404a7bc5e9a8640eb513d9ed2978b
Lucas Felippe <lukzfelippe@gmail.com>
2023-09-30 20:29:20 -0300

    Trabalhando com condições( if e else) e criando um verificador !

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if(vel > 60){ //condição simples//1
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO !`)
+}
+console.log(`Dirija sempre usando o sinto de segurança!`)
